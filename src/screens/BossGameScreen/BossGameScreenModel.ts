1e63066d23e8c753d4f53a32b8384b45aa9521fe
Sandul Henry <sandul.henry.05@gmail.com>
2025-10-28 22:58:13 -0700

    got basic functionality for tiles and tile recognition down

--- src/screens/BossGameScreen/BossGameScreenModel.ts
+++ src/screens/BossGameScreen/BossGameScreenModel.ts
@@ -2,5 +2,25 @@
  * GameScreenModel - Manages game state
  */
 export class BossGameScreenModel {
-	
+	private phases: number = 4;
+    private score: number = 0;
+
+    decrementPhases(): void {
+        this.phases--;
+    }
+
+    getPhases(): number {
+        return this.phases;
+    }
+
+    reset(): void {
+        this.phases = 4;
+		this.score = 0;
+	}
+
+    getScore(): number {
+        return this.score;
+    }
+
+
 }
