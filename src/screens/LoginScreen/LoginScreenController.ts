117cab2b0896742e62994f43192962e44c4fc2a5
Mallika1405 <mallikadasgupta1405@gmail.com>
2025-11-16 11:52:43 -0800

    Login works on the backend

--- src/screens/LoginScreen/LoginScreenController.ts
+++ src/screens/LoginScreen/LoginScreenController.ts
@@ -17,15 +17,61 @@ export class LoginScreenController extends ScreenController {
 
 	// Initialize Model and View
 		this.model = new LoginScreenModel();
-		this.view = new LoginScreenView((username: string) => {
+		this.view = new LoginScreenView(
+			async (username: string, password:string) => {
 			this.model.username = username;
+			this.model.password=password;
 			console.log("Username entered:", this.model.username); // For testing purposes
-		});
-    }
+			if(!username.trim()||!password.trim()){
+				console.error("Username or Password cannot be empty");
+				return;
+			}
+			const response=await fetch("http://localhost:3000/login",{
+				method:"POST",
+				headers:{"Content-Type":"application/json"},
+				body:JSON.stringify({username,password})
+			});
+			const data=await response.json();
+			if(!response.ok){
+				console.error("Login failed:",data.error);
+				return;
+			}
+			console.log("Login Success",data)
+			this.screenSwitcher.switchToScreen({type:"tower_select"});
+			},
+			async (username:string,password:string) =>{
+				this.model.username=username;
+				this.model.password=password;
+
+				if(!username.trim() || !password.trim()){
+					console.error("Username or password cannot be empty");
+					return;
+				}
+
+			
+
+			const response=await fetch('http://localhost:3000/signup',{
+				method:"POST",
+				headers:{"Content-Type":"application/json"},
+				body:JSON.stringify({username,password})
+			});
+
+			const data=await response.json();
+			if(!response.ok){
+				console.error("SignUp failed:",data.error);
+				return;
+			}
+			console.log("SignUp Success:",data)
+			this.screenSwitcher.switchToScreen({type:"tower_select"});
+		
+   	 	}
+	);
+}
 	/**
 	 * Get the view group
 	 */
 	getView(): LoginScreenView {
 		return this.view;
 	}
+	
 }
--- src/screens/LoginScreen/LoginScreenView.ts
+++ src/screens/LoginScreen/LoginScreenView.ts
@@ -8,9 +8,16 @@ import { STAGE_WIDTH, STAGE_HEIGHT } from "../../constants.ts";
 export class LoginScreenView implements View {
 	private group: Konva.Group;
 	private usernameInput: HTMLInputElement
+	private passwordInput:HTMLInputElement
 
-    constructor(onUsernameEntered: (username: string) => void) {
+	private onLoginEntered:(username:string,password:string)=>void;
+	private onSignUpEntered:(username:string,password:string)=>void;
+    constructor(
+		onLoginEntered: (username: string, password:string) => void, 
+		onSignUpEntered:(username:string,password:string)=>void) {
 		this.group = new Konva.Group({ visible: false });
+		this.onLoginEntered=onLoginEntered
+		this.onSignUpEntered=onSignUpEntered
 		/*
 	 	* Title text
 		*/
@@ -29,17 +36,46 @@ export class LoginScreenView implements View {
 		this.usernameInput.type = "text";
 		this.usernameInput.placeholder = "Enter Username";
 		this.usernameInput.style.position = "absolute";
-		this.usernameInput.style.top = `${STAGE_HEIGHT / 2}px`;
+		this.usernameInput.style.top = `${STAGE_HEIGHT / 2 - 40}px`;
 		this.usernameInput.style.left = `${STAGE_WIDTH / 2 - 100}px`;
 		this.usernameInput.style.width = "200px";
 		this.usernameInput.style.fontSize = "16px";
 		document.body.appendChild(this.usernameInput);
 
+		this.passwordInput = document.createElement("input");
+		this.passwordInput.type = "text";
+		this.passwordInput.placeholder = "Enter Password";
+		this.passwordInput.style.position = "absolute";
+		this.passwordInput.style.top = `${STAGE_HEIGHT / 2 - 10}px`;
+		this.passwordInput.style.left = `${STAGE_WIDTH / 2 - 100}px`;
+		this.passwordInput.style.width = "200px";
+		this.passwordInput.style.fontSize = "16px";
+		document.body.appendChild(this.passwordInput);
+
+		const loginButton=document.createElement("button")
+		loginButton.innerText="Login";
+		loginButton.style.position="absolute";
+		loginButton.style.top=`${STAGE_HEIGHT/2+35}px`
+		loginButton.style.left=`${STAGE_WIDTH/2-100}px`
+		loginButton.style.width="90px"
+		loginButton.onclick=()=>
+			this.onLoginEntered(this.usernameInput.value,this.passwordInput.value)
+		document.body.appendChild(loginButton)
+
+		const signUpButton=document.createElement("button")
+		signUpButton.innerText="Sign Up";
+		signUpButton.style.position="absolute";
+		signUpButton.style.top=`${STAGE_HEIGHT/2+100}px`
+		signUpButton.style.left=`${STAGE_WIDTH/2+30}px`
+		signUpButton.style.width="90px"
+		signUpButton.onclick=()=>
+			this.onSignUpEntered(this.usernameInput.value,this.passwordInput.value)
+		document.body.appendChild(signUpButton)
+
 		// Handle Enter key to submit username
-		this.usernameInput.addEventListener("keydown", (e: KeyboardEvent) => {
+		this.passwordInput.addEventListener("keydown", (e: KeyboardEvent) => {
 			if (e.key == "Enter") {
-				const target = e.target as HTMLInputElement;
-				onUsernameEntered(target.value);
+				onLoginEntered(this.usernameInput.value,this.passwordInput.value);
 				
 			}
 		});
