a7bddd23e76fa0fa86f74aa5cd99dd7a3c82df3b
watawada <k3mai@ucsd.edu>
2025-11-24 22:32:48 -0800

    Update enemyFactory.ts

--- src/utils/enemyFactory.ts
+++ src/utils/enemyFactory.ts
@@ -13,7 +13,7 @@ function randomInt(): number {
 function generateBossPhase(): BossPhase {
   return {
         targetNumber: randomInt(),
-        tiles: ['0','1','2','3','4','5','6','7','8','9','-','+','x','/'], 
+        tiles: [], 
         imagePath: `boss_phase_${Math.floor(Math.random() * 3) + 1}.png`
     };
 }
