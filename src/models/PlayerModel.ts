d3d23a0d32654ad5c0cbe5514e17ab770367066c
sandul.henry <sandul.henry.05@gmail.com>
2025-12-04 17:24:42 -0800

    player model fixes

--- src/models/PlayerModel.ts
+++ src/models/PlayerModel.ts
@@ -38,38 +38,8 @@ export class Player {
         this.health -= damage;
     }
 
-    increase_score(change: number): number{
-        //console.log("pkayer model increase score");
-        this.score += change;
-
-        return this.score;
-    }
-
-    decrease_score(change: number): number {
-        this.score -= change;
-        if (this.score < 0) this.score = 0;
-
-        return this.score;
-    }
-
-    get_score(): number { 
-        return this.score;
-    }
-    
-    get_highest_tower(): number {
-        return this.highestTowerUnlocked;
-    }
-
     set_highest_tower(level:number):number{
         return this.highestTowerUnlocked=level;
-    }
-
-    unlock_next_tower(): void {
-        this.highestTowerUnlocked += 1;
-    }
-
-    is_tower_unlocked(tower: number): boolean {
-        return this.highestTowerUnlocked >= tower;
     }
 
     // Score methods
